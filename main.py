9846a3c04c7d69ed6615e5fff2c4873382d7db1c
Mr Mask <ldaiwa7@gmail.com>
2023-11-23 22:06:07 -0500

    window created

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("heko")
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit
+import sys
+
+
+# main window class
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # configuration of the window
+        self.setWindowTitle("nifuji")  # title of window
+        self.setGeometry(100, 100, 800, 600)  # size of window
+
+        # set the main widget
+        self.textEdit = QTextEdit()
+        self.setCentralWidget(self.textEdit)
+
+
+def nifuji():
+    app = QApplication(sys.argv)  # this creates a application object
+    mainWindow = MainWindow()  # creates an instance of the main window
+    mainWindow.show()  # show the actual main window
+    sys.exit(app.exec_())  # starts the applications main loop
+
+
+if __name__ == "__main__":
+    nifuji()
