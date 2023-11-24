779af69cfa6c7bd25a4ec4411f854e7769eb0eb2
Mr Mask <ldaiwa7@gmail.com>
2023-11-23 22:39:33 -0500

    added functions

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTextEdit,
+    QAction,
+    qApp,
+    QFileDialog,
+)
 import sys
 
 
@@ -14,6 +21,43 @@ class MainWindow(QMainWindow):
         # set the main widget
         self.textEdit = QTextEdit()
         self.setCentralWidget(self.textEdit)
+
+        # create menu bar
+        self.createMenuBar()
+
+    def createMenuBar(self):
+        menuBar = self.menuBar()
+
+        # file menu
+        fileMenu = menuBar.addMenu("File")
+
+        # open action
+        openAction = QAction("Open", self)
+        openAction.triggered.connect(self.openFile)
+        fileMenu.addAction(openAction)
+
+        # save action
+        saveAction = QAction("Save", self)
+        saveAction.triggered.connect(self.saveFile)
+        fileMenu.addAction(saveAction)
+
+    def openFile(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(
+            self, "Open File", "", "Text Files (*.txt);;All Files (*)", options=options
+        )
+        if fileName:
+            with open(fileName, "r") as file:
+                self.textEdit.setText(file.read())
+
+    def saveFile(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getSaveFileName(
+            self, "Save File", "", "Text Files (*.txt);;All Files (*)", options=options
+        )
+        if fileName:
+            with open(fileName, "w") as file:
+                file.write(self.textEdit.toPlainText())
 
 
 def nifuji():
